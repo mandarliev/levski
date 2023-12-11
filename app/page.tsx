313ff85f2c5c5cf02d8d05bcb506230dcdff67d2
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-11 17:57:00 +0200

    Initial build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
-  )
+    <main>
+      <h1>Hello</h1>
+    </main>
+  );
 }
